b2d88a93e45b2763691038d5543b22e63defd685
Scott Buchanan <buchanan.sc@gmail.com>
2012-02-20 18:51:22 -0800

    first commit

---
+++ resources/scss/config.rb
@@ -0,0 +1,9 @@
+sass_path = File.dirname(__FILE__)
+css_path = "../css"
+images_dir = "../images"
+
+relative_assets = true
+
+# output_style = :compressed
+output_style = :expanded
+environment = :production
