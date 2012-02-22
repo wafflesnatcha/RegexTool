6dcb62e2a27d190f17dce3db64d576b8e8674fb1
Scott Buchanan <buchanan.sc@gmail.com>
2012-02-21 23:04:44 -0800

    More styling changes.

---
+++ README.md
@@ -0,0 +1,4 @@
+RegexTool
+=========
+
+Inspired by [http://erik.eae.net/playground/regexp/regexp.html](http://erik.eae.net/playground/regexp/regexp.html)
--- resources/scss/config.rb
+++ resources/scss/config.rb
@@ -4,6 +4,5 @@ images_dir = "../images"
 
 relative_assets = true
 
-# output_style = :compressed
-output_style = :expanded
+output_style = :expanded   # :nested, :expanded, :compact, :compressed
 environment = :production
