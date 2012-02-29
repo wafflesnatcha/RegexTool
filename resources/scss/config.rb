a3bf6f6c22b26e8e1440bae40353816a2a6f4288
Scott Buchanan <buchanan.sc@gmail.com>
2012-02-28 20:19:39 -0800

    - tweaked the compass config.rb slightly

--- resources/scss/config.rb
+++ resources/scss/config.rb
@@ -1,9 +1,10 @@
 sass_path = File.dirname(__FILE__)
 css_path = File.join(sass_path, "..", "css")
+javascripts_dir = "../../lib"
 images_dir = "../images"
 
 relative_assets = true
 
 # output_style = :compressed
-output_style = :expanded   # :nested, :expanded, :compact, :compressed
+output_style = :expanded
 environment = :production
